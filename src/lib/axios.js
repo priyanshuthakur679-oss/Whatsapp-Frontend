ed8946c36996143a94fc88db1751a1c0b2ca5c39
priyanshuthakur679-oss <priyanshuthakur679@gmail.com>
2026-01-13 21:22:46 +0530

    update more

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "https://whatsapp-backend.onrender.com",
+  baseURL: "https://whatsapp-backend.onrender.com/api",
   withCredentials: true,
 });
